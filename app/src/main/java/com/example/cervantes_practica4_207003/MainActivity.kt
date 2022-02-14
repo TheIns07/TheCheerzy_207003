d3ef1f4887a0046cd1322b2652cd24796167da2e
Arturo Cervantes <arturo.cer.itson07@gmail.com>
2022-02-13 21:28:13 -0700

    Diseño de gradientes y seleccion de colores

---
+++ app/src/main/java/com/example/cervantes_practica4_207003/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cervantes_practica4_207003
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
