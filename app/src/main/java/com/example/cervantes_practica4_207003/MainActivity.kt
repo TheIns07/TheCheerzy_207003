47146cd9d7955a1446f32dd5bcb27b8dfbcc369f
Arturo Cervantes <arturo.cer.itson07@gmail.com>
2022-02-13 22:37:46 -0700

    Proyecto Terminado: diseño del menu principal y programacion del menu

--- app/src/main/java/com/example/cervantes_practica4_207003/MainActivity.kt
+++ app/src/main/java/com/example/cervantes_practica4_207003/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.cervantes_practica4_207003
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.button_getStarted) as Button
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
